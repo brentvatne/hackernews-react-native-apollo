886d2859c0e79d3725a0ae7ad4ea792d121d8f91
Brent Vatne <brentvatne@gmail.com>
2017-08-26 16:46:34 -0700

    Return success from cron webtask

--- webtasks/cron/index.js
+++ webtasks/cron/index.js
@@ -1,5 +1,5 @@
 require('isomorphic-fetch');
 
 module.exports = function(cb) {
-  return fetch('https://vote-webhook-hrfwlkfoqm.now.sh');
+  return fetch('https://vote-webhook-hrfwlkfoqm.now.sh').then(function(response) { cb(null, 'success') });
 }
